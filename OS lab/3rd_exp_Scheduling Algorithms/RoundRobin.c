7a1b56fafe1bb334d3c4d30eac691ff5fffb5780
MALAPAKA DATTA VENKATA NAGA SAI DEEPAK <154827923+DeepakMalapaka@users.noreply.github.com>
2025-03-14 21:36:53 +0530

    Add files via upload

--- OS lab/3rd_exp_Scheduling Algorithms/RoundRobin.c
+++ OS lab/3rd_exp_Scheduling Algorithms/RoundRobin.c
@@ -1,60 +1,64 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-	int bt[10],wt[10],tat[10],ct[10];
-	int t,max,avgtat,avgwt;
-	int temp=0;
-	clrscr();
-	printf("Enter the number of process:");
-	int n;
-	scanf("%d",&n);
-	printf("Enter the burst times of %d number of process:",n);
-	for(int i=0;i<n;i++)
-	{
-		printf("\nEnter the burst time of process %d: ",i);
-		scanf("%d",&bt[i]);
-		ct[i]=bt[i];
-	}
-	printf("Enter the time quantum for the given problem:");
-	scanf("%d",&t);
-	max=bt[0];
-	for(int i=1;i<n;i++)
-	{
-		if(max<bt[i])
-			max=bt[i];
-	}
-	for(int j=0;j<(max/t);j++)
-	{
-		for(int i=0;i<n;i++)
-		{
-			if(bt[i]!=0)
-			{
-				if(bt[i]<=t)
-				{
-					tat[i]=temp+bt[i];
-					temp=temp+bt[i];
-					bt[i]=0;
-				}
-				else 
-				{
-					bt[i]=bt[i]-t;
-					temp=temp+t;	
-				}
-			}
-		}
-	}
-	for(int i=0;i<n;i++)
-	{
-		wt[i]=tat[i]-ct[i];
-		avgtat+=tat[i];
-		avgwt+=wt[i]
-	}
-	printf(&quot;\nThe Average Turnaround time is -- %f&quot;,att/n);
-	printf(&quot;\nThe Average Waiting time is -- %f &quot;,awt/n);
-	printf(&quot;\n\tPROCESS\t BURST TIME \t WAITING TIME\tTURNAROUND	TIME\n&quot;);
-	for(i=0;i&lt;n;i++)
-		printf(&quot;\t%d \t %d \t\t %d \t\t %d \n&quot;,i+1,ct[i],wa[i],tat[i]);
-	getch();	
-	return 0;
+int main() {
+    int bt[10], wt[10], tat[10], ct[10];
+    int n, t, max, temp = 0;
+    int i;
+    float avgtat = 0, avgwt = 0;
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    printf("Enter the burst times of %d processes:\n", n);
+    for(i = 0; i < n; i++) {
+        printf("Enter the burst time of process %d: ", i + 1);
+        scanf("%d", &bt[i]);
+        ct[i] = bt[i];
+    }
+
+    printf("Enter the time quantum for the given problem: ");
+    scanf("%d", &t);
+    max = bt[0];
+    for (i = 1; i < n; i++) {
+        if (max < bt[i])
+            max = bt[i];
+    }
+
+   
+    int remaining = n; 
+    int completed[10] = {0};
+
+    while (remaining > 0) {
+        for (i = 0; i < n; i++) {
+            if (bt[i] > 0) {
+                if (bt[i] <= t) {
+                    temp += bt[i];
+                    tat[i] = temp;
+                    bt[i] = 0;
+                    completed[i] = 1; 
+                    remaining--;
+                } else {
+                    bt[i] -= t;
+                    temp += t;
+                }
+            }
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        wt[i] = tat[i] - ct[i];
+        avgtat += tat[i];
+        avgwt += wt[i];
+    }
+    printf("\nThe Average Turnaround Time is: %.2f", avgtat / n);
+    printf("\nThe Average Waiting Time is: %.2f\n", avgwt / n);
+    
+    printf("\n\tPROCESS\tBURST TIME\tWAITING TIME\tTURNAROUND TIME\n");
+    for (i = 0; i < n; i++) {
+        printf("\t%d\t%d\t\t%d\t\t%d\n", i + 1, ct[i], wt[i], tat[i]);
+    }
+
+    return 0;
 }
+
